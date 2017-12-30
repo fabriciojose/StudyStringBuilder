14ad2d51e1fb3ef98b3435d60d4035cb9c01cbf7
fabriciojose <fabmus1@gmail.com>
2017-12-30 12:57:18 -0200

    Estudando String Builder

--- StudyStringBuilder/src/Main.java
+++ StudyStringBuilder/src/Main.java
@@ -2,7 +2,22 @@ import java.util.*;
 
 public class Main
 {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args){
+	
+		StringBuilder sql = new StringBuilder();
+		sql.append ("www.somderaiz.com.br");
+		sql.append("somde1978");
+		sql.append("vida");
+		System.out.println(sql);
 	}
-}
+	
+	}
+
+	/*
+	SAIDA
+
+	
+www.somderaiz.com.brsomde1978vida
+
+
+	*/
